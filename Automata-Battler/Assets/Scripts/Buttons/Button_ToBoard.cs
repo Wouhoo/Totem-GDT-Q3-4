d897840aae29e047c625c80dc90884b49ef8561e
Timmerdebim <timmerdebim@gmail.com>
2025-04-21 14:30:47 +0200

    Clean Up (Delete depricated code)

--- Automata-Battler/Assets/Scripts/Buttons/Button_ToBoard.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Button_ToBoard : MonoBehaviour, Interactable
-{
-    public void OnSelect() { }
-    public void OnDeselect() { }
-    public void OnHover() { }
-    public void OnDehover() { }
-}
--- Automata-Battler/Assets/Scripts/Interactable.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public interface Interactable
-{
-    public void OnSelect() { }
-    public void OnDeselect() { }
-    public void OnHover() { }
-    public void OnDehover() { }
-}
