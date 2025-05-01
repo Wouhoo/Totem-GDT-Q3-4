83a162bfd46e9d7a8b431649334b574ae7858c7a
HaentjeGH <l.m.j.haenen@student.tue.nl>
2025-05-01 16:35:28 +0200

    Fix Z-fighting, added shader options, fix card names

--- Automata-Battler/Assets/Shaders/OutlineController.cs
+++ Automata-Battler/Assets/Shaders/OutlineController.cs
@@ -9,6 +9,11 @@ public class OutlineController : MonoBehaviour
     [Tooltip("Index of the material to control the outline effect on, will be inserted in the last slot")]
     [SerializeField] private int materialIndex;
 
+    [Header("Shader settings")]
+    [SerializeField] private bool outlineEnabled = false;
+    [SerializeField] private float width = 1.07f;
+    [SerializeField] private Color color = Color.cyan;
+
     private void Awake()
     {
         //outlineMaterialTemplate = Resources.Load<Material>("Materials/OutlineShader"); //THAT IS THE DUMBEST SHIT I HAVE SEEN ALL WEEK AND I CODE DAFNY FFS WHY IS IT SO SPECIFIC
@@ -31,12 +36,32 @@ public class OutlineController : MonoBehaviour
             _renderer.sharedMaterials = newMaterials;
             materialIndex = newMaterials.Length - 1;
         }
+        //set the staring values
+        _renderer.GetPropertyBlock(_propBlock, materialIndex);
+        _propBlock.SetFloat("_Enabled", outlineEnabled ? 1f : 0f);
+        _propBlock.SetFloat("_XZ_scale", width);
+        _propBlock.SetColor("_Color", color);
+        _renderer.SetPropertyBlock(_propBlock, materialIndex);
     }
 
     public void SetOutline(bool enabled)
     {
         _renderer.GetPropertyBlock(_propBlock, materialIndex);
         _propBlock.SetFloat("_Enabled", enabled ? 1f : 0f);
+        _renderer.SetPropertyBlock(_propBlock, materialIndex);
+    }
+
+    public void SetWidth(float width)
+    {
+        _renderer.GetPropertyBlock(_propBlock, materialIndex);
+        _propBlock.SetFloat("_XZ_scale", width);
+        _renderer.SetPropertyBlock(_propBlock, materialIndex);
+    }
+
+    public void SetColor(Color c)
+    {
+        _renderer.GetPropertyBlock(_propBlock, materialIndex);
+        _propBlock.SetColor("_Color", c);
         _renderer.SetPropertyBlock(_propBlock, materialIndex);
     }
 }
