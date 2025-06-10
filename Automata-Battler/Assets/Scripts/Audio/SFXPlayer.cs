b7454cd91e004480f7f14576db9711dcda842a95
Wouter <wouter@familieleenen.nl>
2025-06-10 16:57:24 +0200

    Lighting fix + SFXPlayer WIP

---
+++ Automata-Battler/Assets/Scripts/Audio/SFXPlayer.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class SFXPlayer : MonoBehaviour
+{
+    public static SFXPlayer Instance { get; private set; }
+
+    [SerializeField] List<AudioClip> soundEffects;
+    private AudioSource audioPlayer;
+
+    public enum SoundEffect
+    {
+        ButtonClick,
+        TurnChange
+        // @Kerem add more here as required
+    }
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+
+        audioPlayer = GetComponent<AudioSource>();
+    }
+
+    public void PlaySoundEffect(SoundEffect soundEffect)
+    {
+        // Play given sound effect. Can be called from anywhere where a sound effect is needed
+        // (this does mean we can't have positional audio, but who fucking cares)
+        try
+        {
+            AudioClip clip = soundEffects[(int)soundEffect];
+            audioPlayer.PlayOneShot(clip);
+        }
+        catch
+        {
+            Debug.LogError("Couldn't find clip corresponding to sound effect!");
+        }
+    }
+}
